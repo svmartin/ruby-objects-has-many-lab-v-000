62a83510c8c4ddf195eb65685a845c8b7dd169d2
Sean Martin <sean.martin@gmail.com>
2019-01-05 18:55:51 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,18 +1,23 @@
 class Artist
-  attr_accessor :name, :songs
+  attr_accessor :name
 
   def initialize(name)
     @name = name
     @songs = []
   end
 
+  def songs
+    @songs
+  end
+
   def add_song(song)
-    self.songs << song
+    @songs.push(song)
     song.artist = self
   end
 
   def add_song_by_name(song_name)
-    #
+    @temp_song = new Song(song_name)
+    self.songs <<
   end
 
   def self.song_count
